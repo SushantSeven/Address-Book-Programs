6c351179c3fea8f4cc50474f0b72f2bdf7a24caa
SushantSeven <sushant.das166@gmai.com>
2024-03-12 21:46:26 +0530

    [Sushant Add: Python script to add new contact to the addressbook]

--- address_book.py
+++ address_book.py
@@ -1,5 +1,27 @@
-# creating address book class
-class Contact():
+# # creating address book class
+# class Contact():
+#     # init method to print the heading
+#     def __init__(self):
+#          # take user details as input
+#         self.first_name = input("Enter your first name: ")
+#         self.address = input("Enter your address: ")
+#         self.last_name = input("Enter your last name: ")
+#         self.city = input("Enter your city: ")
+#         self.state = input("Enter your state: ")
+#         self.zip = input("Enter your zip code: ")
+#         self.phone = input("Enter your phone number: ")
+#         self.email = input("Enter your email: ")
+    
+#     # function to create a contact
+#     def create_contact(self):
+#         user_contact = {"first name":self.first_name,"last name":self.last_name, "address":self.address, "city":self.city, "state": self.state, "zip":zip, "phone":self.phone, "email":self.email}
+#         print(user_contact)
+
+
+# user = Contact() # creating instance of address book class
+# user.create_contact()
+
+class AddressBook():
     # init method to print the heading
     def __init__(self):
          # take user details as input
@@ -11,12 +33,17 @@ class Contact():
         self.zip = input("Enter your zip code: ")
         self.phone = input("Enter your phone number: ")
         self.email = input("Enter your email: ")
+        self.contact_book= []
+        self.user_contact = {}
     
     # function to create a contact
     def create_contact(self):
-        user_contact = {"first name":self.first_name,"last name":self.last_name, "address":self.address, "city":self.city, "state": self.state, "zip":zip, "phone":self.phone, "email":self.email}
-        print(user_contact)
+        self.user_contact = {"first name":self.first_name,"last name":self.last_name, "address":self.address, "city":self.city, "state": self.state, "zip":zip, "phone":self.phone, "email":self.email}
+    
+    def add_new_contact(self):
+        self.contact_book.append(self.user_contact)
+        print(self.contact_book)
 
-
-user = Contact() # creating instance of address book class
+user = AddressBook() # creating instance of address book class
 user.create_contact()
+user.add_new_contact()
