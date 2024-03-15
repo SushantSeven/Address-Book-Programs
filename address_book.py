81c4206824e13af2bb129cebdd3721ad614c021a
SushantSeven <sushant.das166@gmai.com>
2024-03-15 19:17:25 +0530

    [Sushant] Refactor: implemented exception handling

--- address_book.py
+++ address_book.py
@@ -4,8 +4,8 @@ class AddressBook():
     def __init__(self):
          # take user details as input
         self.first_name = input("Enter your first name: ")
-        self.address = input("Enter your address: ")
         self.last_name = input("Enter your last name: ")
+        self.address = input("Enter your address: ")
         self.city = input("Enter your city: ")
         self.state = input("Enter your state: ")
         self.zip = input("Enter your zip code: ")
@@ -14,8 +14,17 @@ class AddressBook():
     
     # function to create a contact
     def create_contact(self):
-        user_contact = {"first name":self.first_name,"last name":self.last_name, "address":self.address, "city":self.city, "state": self.state, "zip":zip, "phone":self.phone, "email":self.email}
-        print(user_contact)
+        try: 
+            user_contact = {"first name":self.first_name,"last name":self.last_name, "address":self.address, "city":self.city, "state": self.state, "zip":self.zip, "phone":self.phone, "email":self.email}
+            print("\n**Contact created**")
+            print("-----------------------------")
+            for key, value in user_contact.items():
+                print("{:<20}".format(key),end="")
+            print("-------------------------------------------------------------------------------------------------------------------------------------------------")
+            for key, value in user_contact.items():
+                print("{:<20}".format(value),end="")
+        except Exception as ex:
+            print(ex)
 
 
 user = AddressBook() # creating instance of address book class
