9abb438beda3dba6b313bfb547ab82bd5a468ada
SushantSeven <sushant.das166@gmai.com>
2024-03-15 19:45:40 +0530

    [Sushant] Refactor: Implemented Exception handling

--- address_book.py
+++ address_book.py
@@ -3,8 +3,8 @@ class AddressBook():
     def __init__(self):
          # take user details as input
         self.first_name = input("Enter your first name: ")
-        self.address = input("Enter your address: ")
         self.last_name = input("Enter your last name: ")
+        self.address = input("Enter your address: ")
         self.city = input("Enter your city: ")
         self.state = input("Enter your state: ")
         self.zip = input("Enter your zip code: ")
@@ -15,13 +15,43 @@ class AddressBook():
     
     # function to create a contact
     def create_contact(self):
-        self.user_contact = {"first name":self.first_name,"last name":self.last_name, "address":self.address, "city":self.city, "state": self.state, "zip":zip, "phone":self.phone, "email":self.email}
-    
+        try:
+            self.user_contact = {"first name":self.first_name,"last name":self.last_name, "address":self.address, "city":self.city, "state": self.state, "zip":self.zip, "phone":self.phone, "email":self.email}
+            print("Contact created!")
+        except:
+            print("Opps!! something went wrong!! please try again in some time.")
     # function to add new contact
     def add_new_contact(self):
-        self.contact_book.append(self.user_contact) # appending new contact to address book
-        print(self.contact_book)
+        try:
+            self.contact_book.append(self.user_contact) # appending new contact to address book
+        except Exception as ex:
+            print(ex)
+        else:
+            for key, value in self.user_contact.items():
+                print("{:<20}".format(key),end="")
+            print("-------------------------------------------------------------------------------------------------------------------------------------------------")
+            for contact in self.contact_book:
+                for key, value in contact.items():
+                    print("{:<20}".format(value),end="")
+                
+def main_menu():
+    try:
+        choice = int(input("Chose your option: "))
+    except ValueError:
+        print("Enter a proper value!")
+    except Exception as ex:
+        print(ex)
+    else:
+        match choice:
+            case 1:
+                print("Hello")
+                user = AddressBook() # creating instance of address book class
+                user.create_contact()
+            case 2:
+                user = AddressBook() # creating instance of address book class
+                user.create_contact()
+                user.add_new_contact()
 
-user = AddressBook() # creating instance of address book class
-user.create_contact()
-user.add_new_contact()
+
+if __name__ == "__main__":
+    main_menu()
