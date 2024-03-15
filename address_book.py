5c2525fc0fc4570f9dac58b4d4b6d02453b91a9f
SushantSeven <sushant.das166@gmai.com>
2024-03-15 21:50:45 +0530

    [Sushant] Refactor: Added menu based functionality

--- address_book.py
+++ address_book.py
@@ -1,41 +1,87 @@
 class AddressBook():
-    # init method to print the heading
-    def __init__(self):
-         # take user details as input
-        self.first_name = input("Enter your first name: ")
-        self.last_name = input("Enter your last name: ")
-        self.address = input("Enter your address: ")
-        self.city = input("Enter your city: ")
-        self.state = input("Enter your state: ")
-        self.zip = input("Enter your zip code: ")
-        self.phone = input("Enter your phone number: ")
-        self.email = input("Enter your email: ")
-        self.contact_book= []
-        self.user_contact = {}
+
+    contact_book= []
+    user_contact = {}
+
+    def print_contact_book(self):
+        for key, value in self.user_contact.items():
+            print("{:<20}".format(key),end="")
+        print("-------------------------------------------------------------------------------------------------------------------------------------------------")
+        for contact in self.contact_book:
+            for key, value in contact.items():
+                print("{:<20}".format(value),end="")
+        print("\n")
     
     # function to create a contact
     def create_contact(self):
-        self.user_contact = {"first name":self.first_name,"last name":self.last_name, "address":self.address, "city":self.city, "state": self.state, "zip":zip, "phone":self.phone, "email":self.email}
+        self.first_name = input("# Enter your first name: ")
+        self.last_name = input("# Enter your last name: ")
+        self.address = input("# Enter your address: ")
+        self.city = input("# Enter your city: ")
+        self.state = input("# Enter your state: ")
+        self.zipp = input("# Enter your zip code: ")
+        self.phone = input("# Enter your phone number: ")
+        self.email = input("# Enter your email: ")
+        self.user_contact = {"first name":self.first_name,"last name":self.last_name, "address":self.address, "city":self.city, "state": self.state, "zip":self.zipp, "phone":self.phone, "email":self.email}
+        
     
     # function to add new contact
     def add_new_contact(self):
-        self.contact_book.append(self.user_contact) # appending new contact to address book
-        print(self.contact_book)
+        self.ch = "y"
+        while self.ch == 'y':
+            print(self.ch)
+            self.create_contact()
+            self.contact_book.append(self.user_contact) # appending new contact to address book
+            print("\ncontact Added!!")
+            self.ch = input("\nPress Y to add another person's details or press X if you are done: ").lower()
+        return self.contact_book
 
     def edit_contact(self):
-        name = input("Enter the name: ")
-        details_to_edited = input("Enter the details to be edited: ").split(" ")
+        self.add_new_contact()
+        print("\n-------EDIT DETAILS-------\n")
+        name = input("ENTER THE NAME: ")
+        presen_or_not = 0
         for contact in self.contact_book:
             if contact["first name"] == name:
-                for x in details_to_edited:
-                    print(f"Enter the new {x}")
-                    contact[x] = input()
+                presen_or_not +=1
+        if presen_or_not == 0:
+            print("Person not present!")
+        else:
+            details_to_edited = input("ENTER THE TO BE EDITED: ").split(",")
+            for contact in self.contact_book:
+                if contact["first name"] == name:
+                    for x in details_to_edited:
+                        contact[x] = input(f"ENTER THE NEW {x}: ")
 
-        print("Edited details are: ")
-        print(self.contact_book)
+            print("\nEDITED DETAILS ARE:\n")
+            self.print_contact_book()
 
-        
-user = AddressBook() # creating instance of address book class
-user.create_contact()
-user.add_new_contact()
-user.edit_contact()
+def main_menu():
+    choice = 1
+    user = AddressBook() # creating instance of address book class
+    while choice != 0:
+            print("\n1. CREATE A CONTACT")
+            print("2. ADD NEW CONTACT")
+            print("3. EDIT NEW CONTACT\n")
+            print("0. EXIT\n")
+            try:
+                choice = int(input("ENTER YOUR OPTION: "))
+            except ValueError:
+                print("Enter a proper value!")
+            except Exception as ex:
+                print(ex)
+            else:
+                match choice:
+                    case 1:
+                        user.create_contact()
+                    case 2:
+                        # user = AddressBook() # creating instance of address book class
+                        user.add_new_contact()
+                    case 3:
+                        # user = AddressBook() # creating instance of address book class
+                        user.edit_contact()
+                    
+
+
+if __name__ == "__main__":
+    main_menu()
