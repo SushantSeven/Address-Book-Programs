358275f84ee1d51948210742b0d4f33ed1dfa9fc
SushantSeven <sushant.das166@gmai.com>
2024-03-16 09:19:49 +0530

    [Sushant] Add: Python script to delete a person's contact from address book

--- address_book.py
+++ address_book.py
@@ -3,13 +3,14 @@ class AddressBook():
     contact_book= []
     user_contact = {}
 
+# method to print the address book
     def print_contact_book(self):
-        for key, value in self.user_contact.items():
+        for key, value in self.user_contact.items(): # print the heading of the address book
             print("{:<20}".format(key),end="")
         print("-------------------------------------------------------------------------------------------------------------------------------------------------")
         for contact in self.contact_book:
             for key, value in contact.items():
-                print("{:<20}".format(value),end="")
+                print("{:<20}".format(value),end="") # print the contacts
         print("\n")
     
     # function to create a contact
@@ -23,13 +24,12 @@ class AddressBook():
         self.phone = input("# Enter your phone number: ")
         self.email = input("# Enter your email: ")
         self.user_contact = {"first name":self.first_name,"last name":self.last_name, "address":self.address, "city":self.city, "state": self.state, "zip":self.zipp, "phone":self.phone, "email":self.email}
-        
+        return self.user_contact
     
     # function to add new contact
     def add_new_contact(self):
         self.ch = "y"
-        while self.ch == 'y':
-            print(self.ch)
+        while self.ch == 'y': # loop untill you finish adding new contact
             self.create_contact()
             self.contact_book.append(self.user_contact) # appending new contact to address book
             print("\ncontact Added!!")
@@ -37,24 +37,35 @@ class AddressBook():
         return self.contact_book
 
     def edit_contact(self):
-        self.add_new_contact()
+        self.add_new_contact() # add new contact is called
         print("\n-------EDIT DETAILS-------\n")
-        name = input("ENTER THE NAME: ")
-        presen_or_not = 0
+        name = input("ENTER THE NAME: ") # name of the user is taken
+        present_or_not = 0
         for contact in self.contact_book:
-            if contact["first name"] == name:
-                presen_or_not +=1
-        if presen_or_not == 0:
+            if contact["first name"] == name: # checking if the user is present in the contact book or not
+                present_or_not +=1
+        if present_or_not == 0:
             print("Person not present!")
         else:
-            details_to_edited = input("ENTER THE TO BE EDITED: ").split(",")
-            for contact in self.contact_book:
-                if contact["first name"] == name:
-                    for x in details_to_edited:
-                        contact[x] = input(f"ENTER THE NEW {x}: ")
+            details_to_edited = input("ENTER THE FIELD TO BE EDITED: ")
+            for contact in self.contact_book: # editing the details
+                    if contact["first name"] == name:
+                        contact[details_to_edited] = input(f"ENTER THE NEW {details_to_edited}: ")
 
             print("\nEDITED DETAILS ARE:\n")
             self.print_contact_book()
+
+    def delete_contact(self):
+        self.add_new_contact() # add new contact method is called
+        conatct_del = input("Enter the name of the contact to be deleted: ") # name of the contact to be deleted is takes
+        for contact in self.contact_book:
+            if conatct_del in contact.values():
+                contact.clear()  # contact is deleted if it is present in the address book
+                print("After deletion-")
+                self.print_contact_book()
+                return
+            else:
+                print("Contact not present")
 
 def main_menu():
     choice = 1
@@ -62,7 +73,8 @@ def main_menu():
     while choice != 0:
             print("\n1. CREATE A CONTACT")
             print("2. ADD NEW CONTACT")
-            print("3. EDIT NEW CONTACT\n")
+            print("3. EDIT CONTACT")
+            print("4. DELETE CONTACT\n")
             print("0. EXIT\n")
             try:
                 choice = int(input("ENTER YOUR OPTION: "))
@@ -75,12 +87,11 @@ def main_menu():
                     case 1:
                         user.create_contact()
                     case 2:
-                        # user = AddressBook() # creating instance of address book class
                         user.add_new_contact()
                     case 3:
-                        # user = AddressBook() # creating instance of address book class
                         user.edit_contact()
-                    
+                    case 4:
+                        user.delete_contact()
 
 
 if __name__ == "__main__":
